10d24896ce8b950e37c0cc9f1ce8543684e4f231
gager98 <gcr001@uark.edu>
2021-02-06 14:21:08 -0600

    created class roster DTO

---
+++ src/main/java/com/example/CapstoneBackend/DTO/ClassRosterDTO.java
@@ -0,0 +1,23 @@
+package com.example.CapstoneBackend.DTO;
+
+public class ClassRosterDTO {
+    String class_id;
+
+    public String getClass_id() {
+        return this.class_id;
+    }
+
+    public void setClass_id(String class_id) {
+        this.class_id = class_id;
+    }
+
+    int user_id;
+
+    public int getUser_id() {
+        return this.user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
