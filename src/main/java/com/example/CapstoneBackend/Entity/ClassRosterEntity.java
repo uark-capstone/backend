9a32271beb46754bdf6aff22ec06122afadb8a47
gager98 <gcr001@uark.edu>
2021-02-06 14:10:13 -0600

    created class roster entity

---
+++ src/main/java/com/example/CapstoneBackend/Entity/ClassRosterEntity.java
@@ -0,0 +1,24 @@
+package com.example.CapstoneBackend.Entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "class_roster")
+public class ClassRosterEntity {
+
+    public ClassRosterEntity(){};
+
+    @Id
+    @Column(name="class_id", updatable = false)
+    public String class_id = "";
+    public int getclass_id() {
+        return this.class_id;
+    }
+
+    @Column(name="user_id", updatable = false)
+    private int user_id;
+
+    public int getuser_id() {
+        return this.user_id;
+    }
+}
