0afeac9ccb6cc418fec420cbdc2692d5f725957a
RyanRau <ryanzrau@gmail.com>
2021-02-09 19:10:09 -0600

    POC aws endpoint

--- src/main/java/com/example/CapstoneBackend/Controllers/AWSController.java
+++ src/main/java/com/example/CapstoneBackend/Controllers/AWSController.java
@@ -1,6 +1,9 @@
 package com.example.CapstoneBackend.Controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
 
 import com.example.CapstoneBackend.Commands.UserCommands;
 import com.example.CapstoneBackend.DTO.AWSDTO;
@@ -21,22 +24,28 @@ public class AWSController {
     @RequestMapping(value = "/image-from-extension", consumes = { MediaType.APPLICATION_JSON_VALUE,
         MediaType.APPLICATION_XML_VALUE }, produces = { MediaType.APPLICATION_JSON_VALUE,
                 MediaType.APPLICATION_XML_VALUE }, method = RequestMethod.POST)
-@ResponseBody
-public ResponseEntity<Object> postBody(@RequestBody AWSDTO awsDTO) {
 
-    try {
-        UserDTO userDTO= userCommands.getUserbyEmail(awsDTO.getID());
-        awsDTO.setID(String.valueOf(userDTO.getId()));
-        System.out.println(awsDTO.toString());
-        return ResponseEntity.status(HttpStatus.OK).body(awsDTO);
+    @ResponseBody
+    public ResponseEntity<Object> postBody(@RequestBody AWSDTO awsDTO) {
 
-    } catch (Exception e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-    }
-
-}
-
-//user and string and timestamp
-
+        try {
+            final String uri = "http://127.0.0.1:5000/rekognition";
+            RestTemplate restTemplate = new RestTemplate();
     
+            HashMap<String, String> map = new HashMap<>();
+                map.put("base64String", awsDTO.getBase64String());
+    
+            String awsResult = restTemplate.postForObject(uri, map, String.class);
+
+            // UserDTO userDTO= userCommands.getUserbyEmail(awsDTO.getID());
+            // awsDTO.setID(String.valueOf(userDTO.getId()));
+            // System.out.println(awsDTO.toString());
+            
+            return ResponseEntity.status(HttpStatus.OK).body(awsResult);
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+
+    }   
 }
